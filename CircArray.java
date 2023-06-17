11edcd494c2dfc5d194289d47e0e1d73e53479f3
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-17 10:20:04 +0530

    Add files via upload

---
+++ CircArray.java
@@ -0,0 +1,73 @@
+package Queue;
+public class CircArray {
+
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
+        }
+
+        public boolean empty(){
+            return rear  == -1 && front == -1;
+        }
+        
+        public static boolean Isfull(){
+           return (rear + 1)%size == front;
+        }
+        public void add(int data){
+            if(Isfull()){
+                System.out.println("Queue is full");
+                return;
+            }
+            if(front == -1){
+                front =0;
+            }
+            rear = (rear + 1)%size;
+            arr[rear] = data;
+        }
+
+        public int remove(){
+            if(empty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+            if(rear == front){
+                rear = front = -1;
+            }
+            else{
+                front = (front + 1)%size;
+                return front;
+            }
+            return result;
+        }
+        public  int peek(){
+            if(empty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+     public static void main(String[] args) {
+          Queue q = new Queue(5);
+          q.add(1);
+          q.add(2);
+          q.add(3);
+          System.out.println(q.remove());
+          q.add(4);
+          System.out.println(q.remove());
+          q.add(5);
+          while(!q.empty()){
+            System.out.println(q.peek());
+            q.remove();
+          }
+     }
+}
