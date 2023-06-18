67bee7fe8f110fa122c6f9baa0ffa114cf255602
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-19 01:13:21 +0530

    Add files via upload

---
+++ StackDequ.java
@@ -0,0 +1,32 @@
+package Queue;
+
+import java.util.*;
+
+public class StackDequ {
+    static class Stack {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void push(int data) {
+            deque.addLast(data);
+        }
+
+        public int pop() {
+            return deque.removeLast();
+        }
+
+        public int peek() {
+            return deque.getLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(4);
+        s.push(8);
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+}
